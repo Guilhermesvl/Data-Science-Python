55fd015ac5a0a0abc07c27a1568aa9a5e59ae84f
Guilhermesvl <guilherme.henriquesvl@gmail.com>
2023-08-08 14:34:48 -0300

    Testando o slicing em imagens

--- Numpy/testeImagem.py
+++ Numpy/testeImagem.py
@@ -1,8 +1,12 @@
 from skimage import io
-import numpy as np
+import numpy as np      #armazena os pixels da imagem
 import matplotlib.pyplot as plt
 
 img = io.imread('Numpy/skimage.webp')
-plt.imshow(img)
-print(img)
-plt.savefig('imagemSalva.png')
+
+print(img.shape)    #Mostra os pixels
+
+novaImagem = img[0:182] #Exibe apenas os elementos 0 a 181 da matriz
+plt.imshow(novaImagem)
+
+plt.savefig('imagemSalva.png')  #Salva a nova imagem cortada
